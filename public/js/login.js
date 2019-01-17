cc54dd3587f83fb054f194bb77ddb51ae30796f5
Arno <39265533+DeviousWolf4@users.noreply.github.com>
2019-01-17 15:02:32 +0100

    added json shnizzle

--- public/js/login.js
+++ public/js/login.js
@@ -8,7 +8,30 @@ function toggleForm() {
   Login.classList.toggle('inactive');
 }
 
+function getFormData(event) {
+  var inputFields = Login.querySelectorAll('input');
+  var formData = {};
+  inputFields.forEach(function (inputField) {
+    formData[inputField.name] = inputField.value;
+  });
+  console.log(formData);
+}
+
+function login(event) {
+  event.preventDefault();
+  var formData = getFormData(Login);
+  var request = new XMLHttpRequest();
+  request.addEventListener('readystatechange', function () {
+    console.log(event);
+  });
+  request.open('POST', 'http://connect4.pienter.space/api/auth/login');
+  request.setRequestHeader('Content-Type', 'application.json');
+  request.send(JSON.stringify(formData));
+  console.log(formData);
+}
+
 BtnRegister.addEventListener('click', toggleForm);
 BtnLogin.addEventListener('click', toggleForm);
+Login.addEventListener('submit', login);
 console.log("123");
 //# sourceMappingURL=login.js.map
--- src/js/login.js
+++ src/js/login.js
@@ -8,6 +8,32 @@ function toggleForm() {
   Login.classList.toggle('inactive');
 }
 
+function getFormData(event){
+  const inputFields = Login.querySelectorAll('input');
+  const formData = {};
+  inputFields.forEach(function(inputField) {
+    formData[inputField.name] = inputField.value;
+  });
+
+  console.log(formData);
+}
+
+function login(event) {
+  event.preventDefault();
+  const formData = getFormData(Login);
+  const request = new XMLHttpRequest();
+  request.addEventListener('readystatechange', function(){
+    console.log(event);
+
+  });
+  request.open('POST', 'http://connect4.pienter.space/api/auth/login');
+  request.setRequestHeader('Content-Type', 'application.json');
+  request.send(JSON.stringify(formData));
+  console.log(formData);
+}
+
+
 BtnRegister.addEventListener('click', toggleForm);
 BtnLogin.addEventListener('click', toggleForm);
+Login.addEventListener('submit', login);
 console.log("123");
