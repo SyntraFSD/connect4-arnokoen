61cc434544b71b6284614cf3652bf9698be808af
Arno <39265533+DeviousWolf4@users.noreply.github.com>
2019-02-15 10:50:22 +0100

    1

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,66 @@
 var mobileDropdown = document.querySelector(".inactive");
 var hamburger = document.querySelector(".Header-links--mobile-hamburger");
+var mainElement = document.querySelector("main");
 hamburger.addEventListener("click", function () {
   mobileDropdown.classList.toggle("inactive");
 });
-var board = [['empty', 'empty', 'empty', 'empty', 'empty', 'empty'], ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'], ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'], ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'], ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'], ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'], ['empty', 'empty', 'empty', 'empty', 'empty', 'empty']];
+var state = {
+  turn: 'yellow',
+  board: [['empty', 'empty', 'empty', 'empty', 'empty', 'empty'], ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'], ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'], ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'], ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'], ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'], ['empty', 'empty', 'empty', 'empty', 'empty', 'empty']]
+};
 
-function drawBoard() {
-  board.forEach(function (row, rowIndex) {
-    var rowElement = document.createElement('div');
-    var board = document.querySelector('#board');
-    rowElement.classList.add('row');
-    row.forEach(function (col, colIndex) {
-      var colElement = document.createElement('div');
-      colElement.classList.add('col', col);
-      colElement.dataset.rowIndex = rowIndex;
-      colElement.dataset.colIndex = colIndex;
-      rowElement.appendChild(colElement);
-    });
-    board.appendChild(rowElement);
-  });
+function generateBoardHtml(board) {
+  return board.reduce(function (html, col, colIndex) {
+    var colHtml = '<div class="col" data-index="' + colIndex + '">';
+    colHtml += col.reduce(function (rowsHtml, row, rowIndex) {
+      return rowsHtml + '<div class="row ' + row + '"></div>';
+    }, '');
+    colHtml += '</div>';
+    return html + colHtml;
+  }, '');
 }
 
-console.log("hjgdj");
-drawBoard();
+function drawBoard(board, turn, htmlElement) {
+  var boardElement = document.createElement('div');
+  boardElement.id = 'board';
+  boardElement.classList.add(turn);
+  boardElement.innerHTML = generateBoardHtml(board);
+  htmlElement.appendChild(boardElement);
+}
+
+function changeTurn(state) {
+  var newState = state;
+
+  if (newState.turn === 'yellow') {
+    newState.turn = 'red';
+  } else {
+    newState.turn = 'yellow';
+  }
+
+  return newState;
+}
+/*const blabla = state.board.reduce(function(acc, col, colIndex){
+  if (acc === false){
+    if (col === 'empty') {
+      return colIndex;
+    }
+  }
+  return acc + colIndex;
+}, false);
+console.log(blabla);*/
+
+
+function checkColumn(event) {
+  var newState = state;
+
+  if (event.target.matches('.col') || event.target.matches('.row')) {
+    var colElement = event.target.closest('.col');
+    var colElementDataset = colElement.dataset.index;
+    console.log(colElementDataset);
+    console.log(newState.board.indexOf(colElementDataset));
+  }
+}
+
+mainElement.addEventListener('click', checkColumn);
+drawBoard(state.board, state.turn, mainElement);
 //# sourceMappingURL=index.js.map
--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,72 @@
 const mobileDropdown = document.querySelector(".inactive");
 const hamburger = document.querySelector(".Header-links--mobile-hamburger");
+const mainElement = document.querySelector("main");
 
 
 hamburger.addEventListener("click", function () {
   mobileDropdown.classList.toggle("inactive");
 });
 
-const board = [
-  ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-  ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-  ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-  ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-  ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-  ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-  ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-];
+const state = {
+  turn: 'yellow',
+  board: [
+    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+  ],
+};
 
-function drawBoard() {
-  board.forEach(function (row, rowIndex) {
-    const rowElement = document.createElement('div');
-    const board = document.querySelector('#board');
-    rowElement.classList.add('row');
-    row.forEach(function (col, colIndex) {
-      const colElement = document.createElement('div');
-      colElement.classList.add('col', col);
-      colElement.dataset.rowIndex = rowIndex;
-      colElement.dataset.colIndex = colIndex;
-      rowElement.appendChild(colElement);
-    });
-    board.appendChild(rowElement);
-  });
+function generateBoardHtml(board) {
+  return board.reduce(function (html, col, colIndex) {
+    let colHtml = '<div class="col" data-index="' + colIndex + '">';
+    colHtml += col.reduce(function (rowsHtml, row, rowIndex) {
+      return rowsHtml + '<div class="row ' + row + '"></div>';
+    }, '');
+    colHtml += '</div>';
+    return html + colHtml;
+  }, '');
 }
-console.log("hjgdj");
-drawBoard();
+
+function drawBoard(board, turn, htmlElement) {
+  const boardElement = document.createElement('div');
+  boardElement.id = 'board';
+  boardElement.classList.add(turn);
+  boardElement.innerHTML = generateBoardHtml(board);
+  htmlElement.appendChild(boardElement);
+}
+
+function changeTurn(state){
+  let newState = state;
+  if (newState.turn === 'yellow') {
+    newState.turn = 'red';
+  } else {
+    newState.turn = 'yellow';
+  }
+  return newState;
+}
+
+/*const blabla = state.board.reduce(function(acc, col, colIndex){
+  if (acc === false){
+    if (col === 'empty') {
+      return colIndex;
+    }
+  }
+  return acc + colIndex;
+}, false);
+console.log(blabla);*/
+
+function checkColumn(event) {
+  let newState = state;
+  if (event.target.matches('.col') || event.target.matches('.row')){
+    const colElement = event.target.closest('.col');
+    const colElementDataset = colElement.dataset.index;
+    console.log(colElementDataset);
+    console.log(newState.board.indexOf(colElementDataset));
+  }
+}
+mainElement.addEventListener('click', checkColumn);
+drawBoard(state.board, state.turn, mainElement);
